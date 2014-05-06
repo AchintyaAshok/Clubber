be904fb0c2487c22605274451e3a9a23d0943795
Achintya Ashok <aa3099@nyu.edu>
2014-05-06 13:20:18 -0400

    first commit for clubber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clubber::Application.config.secret_key_base = '9c057d0a73f917fcfc96078ceb6f335c1e2e5ec858e854cb268dafad8ca6de4e69b41a7e0ca1eea6040d1eb9dc9e6f48fba9e4e8f551231f891c77ac0db54a87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Clubber::Application.config.session_store :cookie_store, key: '_Clubber_session'
