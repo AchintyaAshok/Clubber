60f85aa9d96b9f39636a97e8513024ff1ae353df
Achintya Ashok <aa3099@nyu.edu>
2014-05-08 12:16:46 -0400

    added comments table for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
 	def index
 		@events = Event.all
+		
 		render json: @events
 		#puts @events
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140506222827) do
+ActiveRecord::Schema.define(version: 20140508161143) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "event_id"
+    t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", force: true do |t|
     t.string   "name"
